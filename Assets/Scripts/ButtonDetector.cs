c8a882a88aedc8ba1fb6a6494aaf44375f628b63
Илья <Hostel_2008@mail.ru>
2024-08-17 17:27:35 +0300

    next commit
    Туплю на куротинах, почему то нихера не получается. остановить или запустить.

--- Assets/Scripts/ButtonDetector.cs
+++ Assets/Scripts/ButtonDetector.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonDetector : MonoBehaviour
 {
+    [SerializeField] Button _butten;
+
     public bool Click { get; private set; }
 
+    private void OnEnable()
+    {
+        _butten.onClick.AddListener(ButtonClick);
+    }
 
     private void Start()
     {
         Click = true;    
     }
 
-    public bool ButtonClick()
+    private void ButtonClick()
     {
         if (Click)
             Click = false;
         else
             Click = true;
+    }
 
-        return Click;
+    private void OnDisable()
+    {
+        _butten.onClick.RemoveListener(ButtonClick);
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private TimeKeeper _timeKeeper;
+    [SerializeField] private ButtonDetector _buttonDetector;
+
+    private Coroutine _coroutine;
 
     private float _delay = 0.5f;
+    private bool _detectorReportNumber = false;
 
     private void Start()
     {
         _text.text = "";
-        StartCoroutine(ReportNumber(_delay, _timeKeeper.GiveNumber));
+    //    StartCoroutine(ReportNumber(_delay, _timeKeeper.GiveNumber));
+    }
+
+    private void Update()
+    {
+        if (_detectorReportNumber == false)
+            if (_buttonDetector.Click)
+            {
+                _coroutine = StartCoroutine(ReportNumber(_delay, _timeKeeper.GiveNumber));
+                _detectorReportNumber = true;
+            }
+        else
+            if (_buttonDetector.Click == false)
+            { 
+                StopCoroutine(_coroutine);
+                _detectorReportNumber = false;
+            }
     }
 
     private IEnumerator ReportNumber(float delay, float number)
@@ -35,6 +56,6 @@ public class Timer : MonoBehaviour
 
     private void OnDisable()
     {
-        StopCoroutine(ReportNumber(_delay,_timeKeeper.GiveNumber));
+  //      StopCoroutine(ReportNumber(_delay,_timeKeeper.GiveNumber));
     }
 }
